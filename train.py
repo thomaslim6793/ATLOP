04826ed9f99b7a391d830061147fe51765275766
Hyun Seung Lim <thomaslim6793@gmail.com>
2025-09-19 16:26:05 -0400

    fix

--- train.py
+++ train.py
@@ -86,7 +86,7 @@ def train(args, model, train_features, dev_features, test_features, tokenizer):
                     print(display_test_examples(args, model, test_features, tokenizer, num_examples=5))
                     
                     if dev_score > best_score:
-                        # best_score = dev_score
+                        best_score = dev_score
                         # pred = report(args, model, test_features)
                         # with open("result.json", "w") as fh:
                         #     json.dump(pred, fh)
